06d926dbfe6c09f3b3600656d5cdc182239ba81c
Гриша Тарасенко <fle4er@yandex.ru>
2021-09-07 20:59:04 +0700

    Add registration

--- resolver/help_requister/views.py
+++ resolver/help_requister/views.py
@@ -7,5 +7,5 @@ class GreetingsPage(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['username'] = self.request.user.name
+        context['username'] = self.request.user.username
         return context
