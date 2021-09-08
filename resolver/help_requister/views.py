a936cd73e282e0a8854f704ea1cc48ab6c609f03
Гриша Тарасенко <fle4er@yandex.ru>
2021-09-08 20:56:28 +0700

    Setup login/registration workflow

--- resolver/help_requister/views.py
+++ resolver/help_requister/views.py
@@ -2,10 +2,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class GreetingsPage(LoginRequiredMixin, TemplateView):
+class GreetingsPage(TemplateView):
     template_name = 'greetings.html.j2'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['username'] = self.request.user.username
-        return context
