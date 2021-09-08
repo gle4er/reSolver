7fd725541dd7046e40d619fc473d6972ba69d2c3
Гриша Тарасенко <fle4er@yandex.ru>
2021-09-08 21:49:53 +0700

    Add problem model

--- resolver/help_requister/models.py
+++ resolver/help_requister/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Problem(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(
+        verbose_name='Problem title',
+        max_length=30,
+    )
+
+    class _ProblemCategories(models.IntegerChoices):
+        School = 1
+        Car = 2
+        Medicine = 3
+        Psychology = 4
+        Etc = 5
+
+    category = models.IntegerField(
+        verbose_name='Problem category',
+        choices=_ProblemCategories.choices,
+    )
+
+    description = models.TextField(
+        verbose_name='Problem full description',
+    )
